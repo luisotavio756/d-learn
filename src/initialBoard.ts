3e2716c734c550755ada56f1ba0a132dda78e9e5
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2023-04-15 17:00:21 -0300

    Fixing card order and icons

--- src/initialBoard.ts
+++ src/initialBoard.ts
@@ -9,103 +9,8 @@ const board: Square[] = [
   },
   {
     id: uuid(),
-    players: [
-      {
-        id: 1,
-        name: 'Luis',
-        color: '#9B51E0',
-        score: 0,
-      },
-      {
-        id: 2,
-        name: 'Luis',
-        color: '#9B51E0',
-        score: 0,
-      },
-      {
-        id: 3,
-        name: 'Luis',
-        color: '#9B51E0',
-        score: 0,
-      },
-      {
-        id: 4,
-        name: 'Luis',
-        color: '#9B51E0',
-        score: 0,
-      },
-    ],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
     players: [],
     type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.LuckOrBadLuck,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchPattern,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.LuckOrBadLuck,
   },
   {
     id: uuid(),
@@ -120,42 +25,22 @@ const board: Square[] = [
   {
     id: uuid(),
     players: [],
-    type: SquareTypes.ArchDecisions,
+    type: SquareTypes.ArchPattern,
   },
   {
     id: uuid(),
     players: [],
     type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
   },
   {
     id: uuid(),
     players: [],
     type: SquareTypes.LuckOrBadLuck,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchDecisions,
   },
   {
     id: uuid(),
@@ -170,42 +55,12 @@ const board: Square[] = [
   {
     id: uuid(),
     players: [],
-    type: SquareTypes.ArchDecisions,
+    type: SquareTypes.ArchPattern,
   },
   {
     id: uuid(),
     players: [],
     type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.QualityAttributes,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.ArchDecisions,
-  },
-  {
-    id: uuid(),
-    players: [],
-    type: SquareTypes.LuckOrBadLuck,
   },
   {
     id: uuid(),
@@ -220,7 +75,127 @@ const board: Square[] = [
   {
     id: uuid(),
     players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.LuckOrBadLuck,
+  },
+  {
+    id: uuid(),
+    players: [],
     type: SquareTypes.ArchDecisions,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.LuckOrBadLuck,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchDecisions,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.LuckOrBadLuck,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchDecisions,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.QualityAttributes,
+  },
+  {
+    id: uuid(),
+    players: [],
+    type: SquareTypes.ArchPattern,
   },
   {
     id: uuid(),
