64d92b546fa0824bfd95344dfffd989aba4d10b5
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2023-05-23 21:46:59 -0300

    Creating dashboard page

---
+++ src/pages/Admin/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './Dashboard.styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
