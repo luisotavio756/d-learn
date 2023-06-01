4d8e98b71b02ae4b62f22d1991ecacc267d8b309
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2023-05-31 21:22:02 -0300

    Updating margin

--- src/pages/Landing/Ranking/Ranking.styles.ts
+++ src/pages/Landing/Ranking/Ranking.styles.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import { Flex } from '../../../components/Layout';
+import { breakpoints } from '../../../styles/breakpoints';
 
 export const Container = styled(Flex)`
   max-width: 1400px;
   width: 100%;
-  padding: 6rem 1rem;
+  padding: 6rem 4rem;
   margin: 0 auto;
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    padding: 6rem 1rem;
+  }
 `;
