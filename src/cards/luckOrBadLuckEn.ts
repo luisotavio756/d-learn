5b2e319eebfc3fec2d0f5603629ed0b29ff58b89
Mateus Fonseca Lima <mateus.xfl@gmail.com>
2024-02-26 22:48:14 -0300

    fix: assigning ID directly to lucky cards, to be able to map cards already used when changing language

--- src/cards/luckOrBadLuckEn.ts
+++ src/cards/luckOrBadLuckEn.ts
@@ -1,9 +1,8 @@
-import { v4 as uuid } from 'uuid';
 import { Card, CardTypes } from '../types';
 
 const luckOrBadLuckCardsEn: Card[] = [
   {
-    _id: uuid(),
+    _id: '58a315a5-194f-4f29-8e38-5030d1da1dbd',
     title: 'Luck',
     description: 'Advance three spaces on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -12,7 +11,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 3,
   },
   {
-    _id: uuid(),
+    _id: '261729ac-191b-44ce-80ec-e44054a9c823',
     title: 'Luck',
     description:
       'With this card, you can consult some material before answering a question!\nAdvance one space!',
@@ -22,7 +21,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: '74077878-d41f-4fbf-8756-754280928d56',
     title: 'Luck',
     description: 'Advance two spaces on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -31,7 +30,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '14545891-239b-4ee9-937c-baacb8e00902',
     title: 'Luck',
     description:
       'Your next question will be worth double the stars! \nAdvance one space',
@@ -44,7 +43,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: '1f665b53-af12-4f11-9f45-d07d32210c33',
     title: 'Luck',
     description: 'Advance one space on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -53,7 +52,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: 'c6b9aa01-dc8d-4603-b7b1-509059831561',
     title: 'Luck',
     description:
       'With this card, you can consult some material before answering a question!\nAdvance one space!',
@@ -63,7 +62,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: '74f8c93d-3844-47b4-845f-1725fb1d69ae',
     title: 'Luck',
     description: 'Advance two spaces on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -72,7 +71,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '42d5768b-08d7-4052-9a4e-7636835e9d44',
     title: 'Luck',
     description:
       'Your next question will be worth double the stars! Advance one space',
@@ -85,7 +84,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: '98ff7b39-1adb-40fc-aea9-1832434f4c6c',
     title: 'Bad Luck',
     description: 'Go back three spaces on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -94,7 +93,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 3,
   },
   {
-    _id: uuid(),
+    _id: '24e0b905-5018-407a-8f4f-f47f66945b64',
     title: 'Bad Luck',
     description:
       'Your next question will be worth half the stars! \nGo back one space!',
@@ -107,7 +106,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: 'e3da76c2-811f-4028-8812-fab1a5397fee',
     title: 'Bad Luck',
     description: 'Go back two spaces on the board!',
     type: CardTypes.LuckOrBadLuck,
@@ -116,7 +115,7 @@ const luckOrBadLuckCardsEn: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '3626fe85-54e5-4aeb-9be4-4d513637f1e6',
     title: 'Bad Luck',
     description: 'Go back one space on the board!',
     type: CardTypes.LuckOrBadLuck,
--- src/cards/luckOrBadLuckPt.ts
+++ src/cards/luckOrBadLuckPt.ts
@@ -1,9 +1,8 @@
-import { v4 as uuid } from 'uuid';
 import { Card, CardTypes } from '../types';
 
 const luckOrBadLuckCardsPt: Card[] = [
   {
-    _id: uuid(),
+    _id: '58a315a5-194f-4f29-8e38-5030d1da1dbd',
     title: 'Sorte',
     description: 'Avance três casas no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -12,7 +11,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 3,
   },
   {
-    _id: uuid(),
+    _id: '261729ac-191b-44ce-80ec-e44054a9c823',
     title: 'Sorte',
     description:
       'Com esta carta você pode consultar algum material antes de responder a uma pergunta!\nAvance uma casa!',
@@ -22,7 +21,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: '74077878-d41f-4fbf-8756-754280928d56',
     title: 'Sorte',
     description: 'Avance duas casas no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -31,7 +30,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '14545891-239b-4ee9-937c-baacb8e00902',
     title: 'Sorte',
     description:
       'A sua próxima pergunta valerá o dobro das estrelas! \nAvance uma casa',
@@ -44,7 +43,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: '1f665b53-af12-4f11-9f45-d07d32210c33',
     title: 'Sorte',
     description: 'Avance uma casa no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -53,7 +52,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: 'c6b9aa01-dc8d-4603-b7b1-509059831561',
     title: 'Sorte',
     description:
       'Com esta carta você pode consultar algum material antes de responder a uma pergunta!\nAvance uma casa!',
@@ -63,7 +62,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 1,
   },
   {
-    _id: uuid(),
+    _id: '74f8c93d-3844-47b4-845f-1725fb1d69ae',
     title: 'Sorte',
     description: 'Avance duas casas no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -72,7 +71,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '42d5768b-08d7-4052-9a4e-7636835e9d44',
     title: 'Sorte',
     description:
       'A sua próxima pergunta valerá o dobro das estrelas! Avance uma casa',
@@ -85,7 +84,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: '98ff7b39-1adb-40fc-aea9-1832434f4c6c',
     title: 'Azar',
     description: 'Volte três casas no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -94,7 +93,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 3,
   },
   {
-    _id: uuid(),
+    _id: '24e0b905-5018-407a-8f4f-f47f66945b64',
     title: 'Azar',
     description:
       'A sua próxima pergunta valerá metade das estrelas! \nVolte uma casa!',
@@ -107,7 +106,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     },
   },
   {
-    _id: uuid(),
+    _id: 'e3da76c2-811f-4028-8812-fab1a5397fee',
     title: 'Azar',
     description: 'Volte duas casas no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
@@ -116,7 +115,7 @@ const luckOrBadLuckCardsPt: Card[] = [
     stars: 2,
   },
   {
-    _id: uuid(),
+    _id: '3626fe85-54e5-4aeb-9be4-4d513637f1e6',
     title: 'Azar',
     description: 'Volte uma casa no tabuleiro!',
     type: CardTypes.LuckOrBadLuck,
